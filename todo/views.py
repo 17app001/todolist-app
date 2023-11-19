c92e9c407f123e32529b73f9c88f482e01dea71b
Jerry <17app001@gmail.com>
2023-11-19 15:10:29 +0800

    完成更新功能

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import Todo
 from .forms import TodoForm
+from datetime import datetime
 
 # Create your views here.
 
@@ -9,7 +10,7 @@ def createtodo(request):
     form = TodoForm()
     message = ""
     if request.method == "POST":
-        print(request.POST)
+        # 新增
         form = TodoForm(request.POST)
         todo = form.save(commit=False)
         todo.user = request.user
@@ -20,8 +21,24 @@ def createtodo(request):
 
 
 def viewtodo(request, id):
+    message = ""
     todo = Todo.objects.get(id=id)
-    return render(request, "todo/viewtodo.html", {"todo": todo})
+    form = TodoForm(instance=todo)
+
+    if request.method == "POST":
+        # 更新
+        form = TodoForm(request.POST, instance=todo)
+        todo = form.save(commit=False)
+        if todo.completed:
+            todo.date_completed = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            todo.date_completed = None
+        todo.save()
+        message = "更新todo成功!"
+
+    return render(
+        request, "todo/viewtodo.html", {"todo": todo, "form": form, "message": message}
+    )
 
 
 def todo(request):
