f73d3902cfb1404435261105084f75e009a87186
Your Name <you@example.com>
2023-12-03 20:42:48 +0800

    完成首頁一行三列

--- todolist/settings.py
+++ todolist/settings.py
@@ -84,19 +84,16 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "todolist",
-        "USER": "aws_root",
-        "PASSWORD": "aws_root12345678",
-        "HOST": "database-1.cmkvdrhu5fx4.ap-northeast-1.rds.amazonaws.com",
-        "PORT": "3306",
-    }
-    # todolist
-    # aws_root
-    # aws_toot12345678
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "todolist",
+#         "USER": "aws_root",
+#         "PASSWORD": "aws_rootxxxxxxxx",
+#         "HOST": "database-1.cmkvdrhu5fx4.ap-northeast-1.rds.amazonaws.com",
+#         "PORT": "3306",
+#     }
+# }
 
 
 # Password validation
